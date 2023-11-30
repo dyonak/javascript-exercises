d6597dc10cc618e442a50d390f8a3c71b2fbd204
dyonak <dyonak@gmail.com>
2023-11-30 13:08:25 -0600

    Completed 05_sumAll, all tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(fromNumber, toNumber) {
+    let sumReturn = 0;
 
+    if (typeof fromNumber !== 'number' || fromNumber < 0 
+    || typeof toNumber !== 'number' || toNumber < 0){
+        return 'ERROR';
+    };
+    
+    if (fromNumber > toNumber) [fromNumber, toNumber] = [toNumber, fromNumber];
+
+    for (let index = fromNumber; index <= toNumber; index++) {
+        sumReturn += index;
+    };
+
+    return sumReturn;
 };
 
 // Do not edit below this line
