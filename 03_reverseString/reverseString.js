b1d43cb3d4ad778ae7b1617728bf20659865d975
dyonak <dyonak@gmail.com>
2023-11-30 12:06:58 -0600

    Completed 03_reverseString, all tests passing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = ''
+    for (let i = inputString.length; i >= 0; i--) {
+        reversedString += inputString.slice(i-1, i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
