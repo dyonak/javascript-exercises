d068f5b504124f04a9b21c5a0b2e77026083718a
dyonak <dyonak@gmail.com>
2023-11-30 12:41:55 -0600

    Completed 04_removeFromArray, all tests passing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    itemsToBeRemoved = [];
+    while (arguments.length > itemsToBeRemoved.length + 1) {
+        itemsToBeRemoved.push(arguments[1 + itemsToBeRemoved.length]);
+    };
+    for (const item of itemsToBeRemoved) {
+        let index = inputArray.indexOf(item);
+        if (index != -1){
+            inputArray.splice(index, 1);
+        }
+    };
+    return inputArray;
 };
 
 // Do not edit below this line
