d520606427e64764ae4ab9797b3bd2b6ef4cf84d
dyonak <dyonak@gmail.com>
2023-11-30 11:52:22 -0600

    Completed 02_repeatString, all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, n) {
+    returnString = '';
+    if (n == NaN || n < 0) {
+        return 'ERROR'
+    }
+    while(n > 0) {
+        returnString += inputString;
+        n--;
+    };
+    return returnString;
 };
 
 // Do not edit below this line
