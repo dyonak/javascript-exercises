09ebc2eeb63e0e2862849365c4bde12cba7e142b
dyonak <dyonak@gmail.com>
2023-12-14 12:57:52 -0600

    Added calculator solution, accidentally omitted.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayToSum) {
+	let total = 0;
+  arrayToSum.forEach(num => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayToMultiply) {
+  let result = 1
+  arrayToMultiply.forEach(num => 
+    result *= num
+  );
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) {
+    return 1;
+  };
+
+  result = 1;
+  while (x > 1) {
+    result = result * x;
+    x--;
+  }
+  return result;
 };
 
 // Do not edit below this line
