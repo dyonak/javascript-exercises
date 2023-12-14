2280a26716177dbbbf038a10e329edf301de96c4
dyonak <dyonak@gmail.com>
2023-12-14 14:51:11 -0600

    Completed all problems through #12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    a = 1;
+    b = 1;
+    while (x > 2){
+        temp = b;
+        b = b + a;
+        a = temp;
+        x--;
+    };
+    return b;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        
+        currentYear = new Date().getFullYear();
 
+        oldestAge = !oldest.yearOfDeath ? currentYear - oldest.yearOfBirth 
+        : oldest.yearOfDeath - oldest.yearOfBirth;
+        personAge = !person.yearOfDeath ? currentYear - person.yearOfBirth 
+        : person.yearOfDeath - person.yearOfBirth;
+
+        return oldestAge < personAge ? person : oldest;
+
+    }, people[0]);
 };
 
 // Do not edit below this line
